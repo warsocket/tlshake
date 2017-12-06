d9d67c4e57f9a372222adbd0bbbe43daab1c7979
Bram <warsocket@gmail.com>
2017-12-06 21:36:43 +0100

    Relaxed the licence to Gplv3 (was aGPL)

--- tlshake.py
+++ tlshake.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
-#Tlshake
-#Copyright (C) 2016  Bram Staps
-#
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU Affero General Public License as
-#published by the Free Software Foundation, either version 3 of the
-#License, or (at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU Affero General Public License for more details.
-#
-#You should have received a copy of the GNU Affero General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Tlshake
+# Copyright (C) 2016  Bram Staps
+
+# This file is part of Tlshake.
+# Tlshake is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Tlshake is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Tlshake. If not, see <http://www.gnu.org/licenses/>.
 
 import socket
 import names
